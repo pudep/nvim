ee70d4d529fbc26bf2c581dc10885ae93754f6e3
anoninus <anoninuxt@gmail.com>
2026-04-09 10:52:50 +0530

    minimal

--- lua/user/sys/options.lua
+++ lua/user/sys/options.lua
@@ -1,10 +1,9 @@
-
--- speed 
+-- speed
 vim.o.updatetime = 0
 vim.o.timeoutlen = 100
 vim.o.ttimeoutlen = 0
-vim.o.lazyredraw = true 
-vim.o.swapfile = true 
+vim.o.lazyredraw = true
+vim.o.swapfile = true
 vim.o.confirm = true
 vim.o.visualbell = false
 vim.o.errorbells = false
@@ -18,31 +17,31 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.smartindent = true
 vim.o.textwidth = 0
-vim.o.backspace = "indent,eol,start"
+vim.o.backspace = 'indent,eol,start'
 
 -- Search
 vim.o.incsearch = true
 vim.o.hlsearch = true
 -- Fold
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.lsp.foldexpr()'
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
-vim.cmd("syntax on")
-vim.cmd("filetype plugin indent on")
+vim.cmd('syntax on')
+vim.cmd('filetype plugin indent on')
 
 -- Keymaps: Spelling
 vim.keymap.set('n', '<leader>fw', function()
-    local word = vim.fn.expand('<cword>')
-    local suggestions = vim.fn.spellsuggest(word, 1)
-    
-    if #suggestions > 0 then
-        vim.cmd('normal! ciw' .. suggestions[1])
-        vim.cmd('stopinsert')
-    else
-        vim.notify('No spelling suggestions found', vim.log.levels.WARN)
-    end
+  local word = vim.fn.expand('<cword>')
+  local suggestions = vim.fn.spellsuggest(word, 1)
+
+  if #suggestions > 0 then
+    vim.cmd('normal! ciw' .. suggestions[1])
+    vim.cmd('stopinsert')
+  else
+    vim.notify('No spelling suggestions found', vim.log.levels.WARN)
+  end
 end, { desc = 'Fix spelling (first suggestion)' })
 
 vim.keymap.set('n', '<C-x>s', 'z=', { desc = 'Spelling suggestions' })
--- lua/user/ui/core/statusline.lua
+++ lua/user/ui/core/statusline.lua
@@ -1,88 +1,27 @@
--- current buffer LSP name(s)
-local function lsp_server()
-    local clients = vim.lsp.get_clients({ bufnr = 0 })
+vim.o.showcmdloc = "statusline"
+vim.o.cmdheight=0
 
-    if not clients or #clients == 0 then
-        return '󰌘 No LSP'
-    end
-
-    local names = {}
-    for _, c in ipairs(clients) do
-        table.insert(names, c.name)
-    end
-
-    return '󰌘 ' .. table.concat(names, ', ')
+local function lsp_clients()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #clients == 0 then return "" end
+  local names = {}
+  for _, c in ipairs(clients) do
+    table.insert(names, c.name)
+  end
+  return " [" .. table.concat(names, ", ") .. "]"
 end
 
-local lualine = require('lualine')
+vim.o.statusline = "%!v:lua.Statusline()"
 
-lualine.setup({
-    options = {
-        theme = 'auto',      -- auto-detect colorscheme
-        globalstatus = true, -- single statusline (NvChad style)
-        icons_enabled = true,
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        disabled_filetypes = {
-            statusline = { 'dashboard', 'alpha', 'starter' },
-        },
-    },
+function _G.Statusline()
+  local path = "%F"
+  local modified = "%m"
+  local readonly = "%r"
+  local sep = "%="
+  local showcmd = "%S"        -- pending command/count/macro
+  local coords = "%l,%c"
+  local percent = "%p%%"
+  local lsp = lsp_clients()
 
-    sections = {
-        -- LEFT
-        lualine_a = {
-            { 'mode', icon = '' },
-        },
-
-        lualine_b = {
-            { 'branch', icon = '' },
-            'diff',
-        },
-
-        lualine_c = {
-            {
-                'filename',
-                path = 3, -- relative path => 0 = name , 1 = relative , 2 = absolute , 3rd = shortened
-                symbols = {
-                    modified = '●',
-                    readonly = '',
-                    unnamed = '[No Name]',
-                },
-                color = {
-                    gui = "bold"
-                },
-                shorting_target = 40,
-            },
-        },
-
-        -- RIGHT
-        lualine_x = {
-            {
-                lsp_server,
-            },
-            {
-                'diagnostics',
-                sources = { 'nvim_lsp' },
-                symbols = {
-                    error = ' ',
-                    warn  = ' ',
-                    info  = ' ',
-                    hint  = '󰌵 ',
-                },
-            },
-            'filetype',
-        },
-        lualine_y = {
-            'progress',
-        },
-
-        lualine_z = {
-            { 'location', icon = '󰆾' },
-        },
-    },
-
-    inactive_sections = {
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-    },
-})
+  return path .. modified .. readonly .. sep .. showcmd .. "  " .. lsp .. "  " .. coords .. "  " .. percent .. " "
+end
