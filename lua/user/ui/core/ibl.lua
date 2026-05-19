84364a4cc35db5962735b92c68ff30b8a22b31c1
anoninus <anoninuxt@gmail.com>
2026-05-19 20:21:27 +0530

    formatted ibl.lua

--- lua/user/ui/core/ibl.lua
+++ lua/user/ui/core/ibl.lua
@@ -1,50 +1,50 @@
- require('ibl').setup({
-     indent = {
-         char = '│',  -- or '▏' for thinner, '┊' for dotted
-         tab_char = '│',
-         highlight = { 'IblIndent' },
-         smart_indent_cap = true,
-     },
+require('ibl').setup({
+    indent = {
+        char = '│', -- or '▏' for thinner, '┊' for dotted
+        tab_char = '│',
+        highlight = { 'IblIndent' },
+        smart_indent_cap = true,
+    },
 
-     whitespace = {
-         remove_blankline_trail = true,
-     },
+    whitespace = {
+        remove_blankline_trail = true,
+    },
 
-     scope = {
-         enabled = false,  -- Let mini.indentscope handle this
-     },
+    scope = {
+        enabled = false,  -- Let mini.indentscope handle this
+    },
 
-     exclude = {
-         filetypes = {
-             'help', 'dashboard', 'neo-tree', 'Trouble', 'trouble',
-             'lazy', 'mason', 'notify', 'toggleterm', 'lazyterm',
-             'packer', 'checkhealth', 'man', 'gitcommit',
-             'TelescopePrompt', 'TelescopeResults', 'lspinfo',
-             'alpha', 'starter', '',
-         },
-         buftypes = {
-             'terminal', 'nofile', 'quickfix', 'prompt',
-         },
-     },
- })
+    exclude = {
+        filetypes = {
+            'help', 'dashboard', 'neo-tree', 'Trouble', 'trouble',
+            'lazy', 'mason', 'notify', 'toggleterm', 'lazyterm',
+            'packer', 'checkhealth', 'man', 'gitcommit',
+            'TelescopePrompt', 'TelescopeResults', 'lspinfo',
+            'alpha', 'starter', '',
+        },
+        buftypes = {
+            'terminal', 'nofile', 'quickfix', 'prompt',
+        },
+    },
+})
 
 require('mini.indentscope').setup({
     draw = {
         delay = 50,
         animation = require('mini.indentscope').gen_animation.none(),
     },
-    
-    mappings = {
-      -- Textobjects
-      object_scope = 'is',
-      object_scope_with_border = 'as',
 
-      -- Motions (jump to respective border line; if not present - body line)
-      goto_top = '[s',
-      goto_bottom = ']s',
+    mappings = {
+        -- Textobjects
+        object_scope = 'is',
+        object_scope_with_border = 'as',
+
+        -- Motions (jump to respective border line; if not present - body line)
+        goto_top = '[s',
+        goto_bottom = ']s',
     },
-    symbol = '│',  -- matches ibl for consistency
-    
+    symbol = '│', -- matches ibl for consistency
+
     options = {
         try_as_border = true,
         indent_at_cursor = true,
