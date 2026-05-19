28ac8932e54de48db1c1ddb4437be1235e3a0612
anoninus <anoninuxt@gmail.com>
2026-05-19 20:19:48 +0530

    fixed comments toggeling

--- lua/user/specs/editor.lua
+++ lua/user/specs/editor.lua
@@ -18,9 +18,14 @@ return {
     },
     {
         'numToStr/Comment.nvim',
-        keys = {
-            { 'gcc', mode = 'n',          desc = 'Comment line' },
-            { 'gc',  mode = { 'n', 'v' }, desc = 'Comment' },
-        },
+        config = function ()
+            require('Comment').setup({
+                ignore = '^$',
+                toggler = {
+                    line = 'gcc',
+                    block = 'gbc',
+                },
+            })
+        end
     },
 }
