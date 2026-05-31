69bf2dca1b4a296a60f6cdd317641b218b0cb541
anoninus <anoninuxt@gmail.com>
2026-05-31 14:54:38 +0530

    removed treesitter filetype autocmd

--- lua/user/specs/treesitter.lua
+++ lua/user/specs/treesitter.lua
@@ -16,19 +16,10 @@ return {
                 'go', 'json', 'yaml',
             }):wait(300000)
 
-            vim.api.nvim_create_autocmd('FileType', {
-                pattern = {
-                    'html', 'css', 'javascript', 'typescript', 'tsx',
-                    'go', 'json', 'yaml', 'bash',
-                },
-                callback = function()
-                    vim.treesitter.start()
-                    vim.wo[0][0].foldexpr   = 'v:lua.vim.treesitter.foldexpr()'
-                    vim.wo[0][0].foldmethod = 'expr'
-                    vim.wo[0][0].foldlevel  = 99
-                    vim.bo.indentexpr       = "v:lua.require'nvim-treesitter'.indentexpr()"
-                end,
-            })
+            vim.wo[0][0].foldexpr   = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo[0][0].foldmethod = 'expr'
+            vim.wo[0][0].foldlevel  = 99
+            vim.bo.indentexpr       = "v:lua.require'nvim-treesitter'.indentexpr()"
         end
     },
 }
