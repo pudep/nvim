c141841f5a15c6d055cd383a0c9f15d8518b99b8
anoninus <anoninuxt@gmail.com>
2026-05-31 20:56:05 +0530

    env

--- lua/user/sys/env.lua
+++ lua/user/sys/env.lua
@@ -1,6 +1,11 @@
--- Termux only 
+-- Termux only
 vim.env.PATH = table.concat({
-    "/data/data/com.termux/files/usr/bin", vim.env.HOME .. "/.cargo/bin",
+    "/data/data/com.termux/files/usr/bin",
+    vim.env.HOME .. "/.cargo/bin",
     vim.env.HOME .. "/.npm-global/bin",
+    vim.env.HOME .. "/go/bin",
     vim.env.PATH,
 }, ":")
+
+vim.env.GOPATH = vim.env.HOME .. "/go"
+vim.env.GOROOT = "/data/data/com.termux/files/usr/lib/go"
