b2593fe7c299267607046989712d4cbdb8324cab
anoninus <anoninuxt@gmail.com>
2026-06-01 14:04:49 +0530

    Readded nvim-surround to make s work normally

--- lua/user/specs/editor.lua
+++ lua/user/specs/editor.lua
@@ -3,45 +3,28 @@
 -- ===========================
 return {
     {
-        "echasnovski/mini.surround",
-        version = "*",
-        keys = {
-            { "sa", mode = { "n", "v" } },
-            { "sd", mode = { "n", "v" } },
-            { "sr", mode = { "n", "v" } },
-            { "sf", mode = { "n", "v" } },
-            { "sF", mode = { "n", "v" } },
-            { "sh", mode = { "n", "v" } },
-            { "sn", mode = "n" },
-        },
-        opts = {
-            mappings = {
-                add = "sa",
-                delete = "sd",
-                replace = "sr",
-                find = "sf",
-                find_left = "sF",
-                highlight = "sh",
-                update_n_lines = "sn",
-            }
-        },
+        'kylechui/nvim-surround',
+        keys = { 'ys', 'ds', 'cs', { 'S', mode = 'v' } },
+        config = function()
+            require('nvim-surround').setup({})
+        end,
     },
     {
         "akinsho/toggleterm.nvim",
         version = "*",
-        cmd = "ToggleTerm",     -- also loaded on command
-        keys = { "<A-t>" },     -- lazy-load trigger
+        cmd = "ToggleTerm", -- also loaded on command
+        keys = { "<A-t>" }, -- lazy-load trigger
         opts = {
             size = 15,
             open_mapping = "<A-t>",
             hide_numbers = true,
-            shade_terminals = false,     -- keep this false because is  heavy
+            shade_terminals = false, -- keep this false because is  heavy
             -- shading_factor = 2,
             start_insert = true,
-            insert_mappings = false,     -- tt works in insert mode too
+            insert_mappings = false, -- tt works in insert mode too
             terminal_mappings = true,
             persist_size = true,
-            direction = "horizontal",     -- no floating, ever
+            direction = "horizontal", -- no floating, ever
             close_on_exit = true,
             shell = vim.o.shell,
             auto_scroll = true,
