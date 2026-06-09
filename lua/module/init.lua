a8ba8c4e9faede81836d5f4deff26bdd05f7ae49
anoninus <anoninuxt@gmail.com>
2026-06-09 17:10:55 +0530

    clang config for cpp 20 and made terminal config load at startup

--- lua/module/init.lua
+++ lua/module/init.lua
@@ -1,6 +1,5 @@
 -- lua/module/init.lua
 require("module.autosave.config")
+require("module.terminal.config")
 -- require("module.run.config")
 
-vim.keymap.set("n", "<F1>",  function() require("module.terminal.config") end)
-vim.keymap.set("n", "<F12>", function() require("module.terminal.config") end)
--- lua/server/LowLevel/clang.lua
+++ lua/server/LowLevel/clang.lua
@@ -1,6 +1,22 @@
--- user/config/server/LowLevel/clang.lua
-vim.lsp.config('clangd', {
-  cmd       = { 'clangd', '--background-index', '--clang-tidy', '--log=error' },
-  filetypes = { 'c', 'cpp' },
+vim.lsp.config("clangd", {
+    cmd = {
+        "clangd",
+        "--clang-tidy=false",
+        "--background-index=false",
+        "--log=error",
+        "--completion-style=detailed",
+        "--fallback-style=llvm",
+        "--header-insertion=iwyu",
+        "-j=1",
+        "--pch-storage=memory",
+    },
+    flags = {
+        debounce_text_changes = 150,  -- ms after you stop typing before LSP gets the update
+    },
+    filetypes = { "c", "cpp" },
+    init_options = {
+        fallbackFlags = { "-std=c++20" },
+    },
 })
-vim.lsp.enable('clangd')
+
+vim.lsp.enable("clangd")
