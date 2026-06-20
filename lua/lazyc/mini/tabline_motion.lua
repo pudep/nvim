44f7d10152307322a0c27eca90f5b871d9139879
anoninus <anoninuxt@gmail.com>
2026-06-20 19:44:32 +0530

    added <C-c> based tabonly! execution

--- lua/lazyc/mini/tabline_motion.lua
+++ lua/lazyc/mini/tabline_motion.lua
@@ -4,6 +4,8 @@ vim.keymap.set(modes, "<PageDown>", "<cmd>bprevious<cr>", { silent = true, desc 
 vim.keymap.set(modes, "<PageUp>", "<cmd>bnext<cr>", { silent = true, desc = "Next buffer" })
 vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>tabprevious<cr>", { silent = true, desc = "Previous tab" })
 vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>tabnext<cr>", { silent = true, desc = "Next tab" })
+
+vim.keymap.set({"n", "v", "i"}, "<C-c>", "<cmd>tabonly!<cr>", {silent = true, desc = "Close zombie tabs"})
 -- Buffer reordering — mini.tabline has no move commands,
 -- these swap via bufferline order workaround using native cmds
 vim.keymap.set("n", "<A-,>", function()
